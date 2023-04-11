df5200db705cc61619dfbe486aea89bdbde7dac3
m0v3m1n <126737225+m0v3m1n@users.noreply.github.com>
2023-04-11 11:03:45 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup (
+  include_package_data = True ,
+  name = m0v3m1n_Calc_Prac,
+  version = '0.0.1',
+  description = 'OSS_Dev Practice - Calculator',
+  author = 'DMLee',
+  url = 'https://github.com/m0v3m1n/OSS_developing',
+  download_url = 'https://github.com/m0v3m1n/OSS_developing',
+  packages = ['mycalc'],
+  classifiers = ["Programming Language :: Python :: 3"]
+  
+)
